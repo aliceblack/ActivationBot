aa29b91a7f91c5f35db1009ae4db71452744fba0
aliceblack <blackfluid44@gmail.com>
2018-04-04 19:02:04 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,32 @@
+<?php
+
+echo "Hi! This is a bot for Telegram!";
+
+$botToken=getenv('BOT_TOKEN');
+$website="https://api.telegram.org/bot".$botToken;
+$update=file_get_contents("php://input");
+$update=json_decode($update, TRUE);
+$chatId=$update["message"]["chat"]["id"];
+$name=$update["message"]["chat"]["first_name"];
+$text=$update["message"]["text"];
+
+$appToken = getenv('APP_TOKEN');
+
+if(strpos($text,'reply')!==false){
+	file_get_contents($website."/sendmessage?chat_id=".$chatId."&text=hi there");
+}
+
+if(strpos($text,'/activate')!==false){
+	file_get_contents($website."/sendmessage?chat_id=".$chatId."&text=i am awake");
+}
+
+if(strpos($text,'$appToken')!==false){
+	file_get_contents($website."/sendmessage?chat_id=".$chatId."&text=token is correct");
+}
+
+
+$channelUserName="@activationchanneltest";
+
+if(strpos($text,'/activate-channel')!==false){
+	file_get_contents($website."/sendmessage?".$channelUserName."=".$chatId."&text=speaking to the masses");
+}
